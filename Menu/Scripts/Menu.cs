9576ec9be70c704bf57c22d2e4e9fd529b092e6b
Greg <dev@hwaetgames.com>
2021-09-24 13:11:57 -0400

    Initial commit

---
+++ Menu/Scripts/Menu.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Linq;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasGroup))]
+public class Menu : MonoBehaviour
+{
+	public bool menuDisabledOnStart = false;
+	List<CanvasGroup> canvasGroups;
+
+	public GameObject lastSelectedButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		this.GetComponent<RectTransform>().position = Vector3.zero;
+		this.GetComponent<RectTransform>().anchoredPosition = Vector3.zero;
+
+		getCanvasGroups();
+
+		if (menuDisabledOnStart == true)
+		{
+			DisableAll();
+		}
+    }
+
+	private void OnValidate()
+	{
+		getCanvasGroups();
+	}
+
+	// Update is called once per frame
+	void getCanvasGroups()
+    {
+		canvasGroups = this.gameObject.GetComponentsInChildren<CanvasGroup>().ToList();
+	}
+
+	public void Disable(CanvasGroup cg)
+	{
+		cg.interactable = false;
+		cg.alpha = 0;
+		cg.blocksRaycasts = false;
+	}
+
+	public void Enable(CanvasGroup cg)
+	{
+		cg.interactable = true;
+		cg.alpha = 1;
+		cg.blocksRaycasts = true;
+	}
+
+	[ContextMenu("Disable All")]
+	public void DisableAll()
+	{
+		lastSelectedButton = EventSystem.current.currentSelectedGameObject;
+
+		for (int i = 0; i < canvasGroups.Count; i++)
+		{
+			Disable(canvasGroups[i]);
+		}
+	}
+
+	[ContextMenu("Enable All")]
+	public void EnableAll()
+	{
+		if (lastSelectedButton==null)
+		{
+			lastSelectedButton = GetComponentInChildren<Selectable>().gameObject;
+		}
+
+		StartCoroutine(SetNewSelection());
+			
+		for (int i = 0; i < canvasGroups.Count; i++)
+		{
+			Enable(canvasGroups[i]);
+		}
+
+
+	}
+
+	IEnumerator SetNewSelection()
+	{
+		EventSystem.current.SetSelectedGameObject(null);
+
+		yield return null;
+
+		EventSystem.current.SetSelectedGameObject(lastSelectedButton);
+		Selectable selectable = lastSelectedButton.GetComponent<Selectable>();
+		selectable.OnSelect(null);
+	}
+
+
+	public void setVisible(bool visFlag)
+	{
+		if (visFlag == true) EnableAll();
+		if (visFlag == false) DisableAll();
+	}
+
+	public void switchToMenu(Menu menu)
+	{
+		this.DisableAll();
+		menu.EnableAll();
+	}
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+
+# Package
+A Simple Menu Manageement component for unity. Has a function that will target and switch to other menus, and will create and control an attached canvas group. With this all menu panes can be viewed and authored simultaneously, and in play mode: all menus will be aligned with the canvas and disabled as inidicated.
+
+
